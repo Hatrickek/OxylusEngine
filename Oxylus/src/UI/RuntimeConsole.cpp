338fb157ae6014195f28e8731240470becbd175e
Halim <halimbayka@outlook.com>
2023-07-09 10:30:10 +0300

    Better command parsing and registering commands for RuntimeConsole

--- Oxylus/src/UI/RuntimeConsole.cpp
+++ Oxylus/src/UI/RuntimeConsole.cpp
@@ -10,22 +10,22 @@
 namespace Oxylus {
   static ImVec4 GetColor(const spdlog::level::level_enum level) {
     switch (level) {
-      case spdlog::level::info: return {0, 1, 0, 1};
-      case spdlog::level::warn: return {0.2f, 0, 0, 1};
-      case spdlog::level::err: return {1, 0, 0, 1};
-      case spdlog::level::trace: return {1, 1, 1, 1};
+      case spdlog::level::level_enum::info: return {0, 1, 0, 1};
+      case spdlog::level::level_enum::warn: return {0.2f, 0, 0, 1};
+      case spdlog::level::level_enum::err: return {1, 0, 0, 1};
+      case spdlog::level::level_enum::trace: return {1, 1, 1, 1};
       default: return {1, 1, 1, 1};
     }
   }
 
   static const char8_t* GetLevelIcon(spdlog::level::level_enum level) {
     switch (level) {
-      case spdlog::level::trace: return ICON_MDI_MESSAGE_TEXT;
-      case spdlog::level::debug: return ICON_MDI_BUG;
-      case spdlog::level::info: return ICON_MDI_INFORMATION;
-      case spdlog::level::warn: return ICON_MDI_ALERT;
-      case spdlog::level::err: return ICON_MDI_CLOSE_OCTAGON;
-      case spdlog::level::critical: return ICON_MDI_ALERT_OCTAGRAM;
+      case spdlog::level::level_enum::trace: return ICON_MDI_MESSAGE_TEXT;
+      case spdlog::level::level_enum::debug: return ICON_MDI_BUG;
+      case spdlog::level::level_enum::info: return ICON_MDI_INFORMATION;
+      case spdlog::level::level_enum::warn: return ICON_MDI_ALERT;
+      case spdlog::level::level_enum::err: return ICON_MDI_CLOSE_OCTAGON;
+      case spdlog::level::level_enum::critical: return ICON_MDI_ALERT_OCTAGRAM;
       default: ;
     }
 
@@ -44,14 +44,34 @@ namespace Oxylus {
     // Default commands
     RegisterCommand("quit", "", [] { Application::Get()->Close(); });
     RegisterCommand("clear", "", [this] { ClearLog(); });
+    RegisterCommand("help", "", [this] { HelpCommand(); });
+
+    m_RequestScrollToBottom = true;
   }
 
-  void RuntimeConsole::RegisterCommand(const std::string& command, const std::string& onSuccesLog, const std::function<void()>& action) {
-    m_CommandMap.emplace(command, ConsoleCommand{action, onSuccesLog});
+  void RuntimeConsole::RegisterCommand(const std::string& command,
+                                       const std::string& onSuccesLog,
+                                       const std::function<void()>& action) {
+    m_CommandMap.emplace(command, ConsoleCommand{nullptr, nullptr, nullptr, action, onSuccesLog});
+  }
+
+  void RuntimeConsole::RegisterCommand(const std::string& command, const std::string& onSuccesLog, int32_t* value) {
+    m_CommandMap.emplace(command, ConsoleCommand{value, nullptr, nullptr, nullptr, onSuccesLog});
+  }
+
+  void RuntimeConsole::RegisterCommand(const std::string& command, const std::string& onSuccesLog, std::string* value) {
+    m_CommandMap.emplace(command, ConsoleCommand{nullptr, value, nullptr, nullptr, onSuccesLog});
+  }
+
+  void RuntimeConsole::RegisterCommand(const std::string& command, const std::string& onSuccesLog, bool* value) {
+    m_CommandMap.emplace(command, ConsoleCommand{nullptr, nullptr, value, nullptr, onSuccesLog});
   }
 
   void RuntimeConsole::AddLog(const char* fmt, spdlog::level::level_enum level) {
-    m_TextBuffer.emplace_back(ConsoleText{fmt});
+    if ((int32_t)m_TextBuffer.size() >= MAX_TEXT_BUFFER_SIZE)
+        m_TextBuffer.erase(m_TextBuffer.begin());
+    m_TextBuffer.emplace_back(ConsoleText{fmt, level});
+    m_RequestScrollToBottom = true;
   }
 
   void RuntimeConsole::ClearLog() {
@@ -61,7 +81,11 @@ namespace Oxylus {
   void RuntimeConsole::OnImGuiRender(ImGuiWindowFlags windowFlags) {
     if (!Visible)
       return;
-    const auto name = fmt::format(" {} {}\t\t###{}{}", StringUtils::FromChar8T(ICON_MDI_CONSOLE), PanelName, 0, PanelName);
+    const auto name = fmt::format(" {} {}\t\t###{}{}",
+      StringUtils::FromChar8T(ICON_MDI_CONSOLE),
+      PanelName,
+      0,
+      PanelName);
     if (ImGui::Begin(name.c_str(), &Visible, windowFlags)) {
       if (RenderMenuBar) {
         if (ImGui::BeginMenuBar()) {
@@ -81,11 +105,13 @@ namespace Oxylus {
         [[maybe_unused]] ImGuiScoped::StyleVar style1(ImGuiStyleVar_CellPadding, {1, 1});
         if (ImGui::BeginTable("ScrollRegionTable", 1, tableFlags)) {
           width = ImGui::GetWindowSize().x;
+          ImGui::PushFont(Application::Get()->GetImGuiLayer()->BoldFont);
           for (auto& text : m_TextBuffer) {
             text.Render();
           }
-          if (m_RequestScrollToBottom && ImGui::GetScrollMaxY() > 0) {
-            ImGui::SetScrollY(ImGui::GetScrollMaxY());
+          ImGui::PopFont();
+          if (m_RequestScrollToBottom) {
+            ImGui::SetScrollY(ImGui::GetScrollMaxY() * 10);
             m_RequestScrollToBottom = false;
           }
           ImGui::EndTable();
@@ -149,15 +175,48 @@ namespace Oxylus {
   }
 
   void RuntimeConsole::ProcessCommand(const std::string& command) {
-    if (m_CommandMap.contains(command)) {
-      const auto& c = m_CommandMap[command];
-      c.Action();
+    const auto parsedCommand = ParseCommand(command);
+    if (m_CommandMap.contains(parsedCommand)) {
+      const auto& c = m_CommandMap[parsedCommand];
+      if (c.Action != nullptr) {
+        c.Action();
+      }
+      const auto value = ParseValue(command);
+      if (!value.StrValue.empty()) {
+        if (c.StrValue != nullptr) {
+          *c.StrValue = value.StrValue;
+        }
+        else if (c.IntValue != nullptr) {
+          const auto v = value.As<int32_t>();
+          if (v.has_value()) {
+            *c.IntValue = v.value();
+          }
+        }
+        else if (c.BoolValue != nullptr) {
+          const auto v = value.As<int32_t>();
+          if (v.has_value()) {
+            *c.BoolValue = v.value();
+          }
+        }
+      }
       if (!c.OnSuccesLog.empty())
         AddLog(c.OnSuccesLog.c_str(), spdlog::level::level_enum::info);
     }
     else {
       AddLog("Non existent command.", spdlog::level::level_enum::err);
     }
+  }
+
+  RuntimeConsole::ParsedCommandValue RuntimeConsole::ParseValue(const std::string& command) {
+    if (!command.contains(' '))
+      return {""};
+    const auto offset = command.find(' ');
+    const auto value = command.substr(offset + 1, command.size() - offset);
+    return {value};
+  }
+
+  std::string RuntimeConsole::ParseCommand(const std::string& command) {
+    return command.substr(0, command.find(' '));
   }
 
   int RuntimeConsole::InputTextCallback(ImGuiInputTextCallbackData* data) {
@@ -183,5 +242,14 @@ namespace Oxylus {
     }
 
     return 0;
+  }
+
+  void RuntimeConsole::HelpCommand() {
+    std::string availableCommands = "Available commands: \n";
+    for (auto& [commandStr, command] : m_CommandMap) {
+      availableCommands.append(fmt::format("\t{0} \n", commandStr));
+    }
+
+    AddLog(availableCommands.c_str(), spdlog::level::level_enum::trace);
   }
 }
--- Oxylus/src/UI/RuntimeConsole.h
+++ Oxylus/src/UI/RuntimeConsole.h
@@ -2,12 +2,12 @@
 
 #include <imgui.h>
 #include <functional>
+
 #include "Utils/Log.h"
 
 namespace Oxylus {
   class RuntimeConsole {
   public:
-
     bool RenderMenuBar = false;
     bool SetFocusToKeyboardAlways = false;
     const char* PanelName = "RuntimeConsole";
@@ -16,11 +16,29 @@ namespace Oxylus {
     RuntimeConsole();
     ~RuntimeConsole() = default;
 
-    void RegisterCommand(const std::string& command, const std::string& onSuccesLog, const std::function<void()>& action);
+    // Call the `action` when the "command" is called.
+    void RegisterCommand(const std::string& command,
+                         const std::string& onSuccesLog,
+                         const std::function<void()>& action);
+
+    /// Change the `value` with the `command` \n
+    /// example: "command 100" -> "command" is now 100
+    void RegisterCommand(const std::string& command, const std::string& onSuccesLog, int32_t* value);
+
+    /// Change the `value` with the `command` \n
+    /// example: "command text" -> "command" is now text
+    void RegisterCommand(const std::string& command, const std::string& onSuccesLog, std::string* value);
+
+    /// Change the `value` with the `command` \n
+    /// example: "command 1" -> "command" is now true
+    void RegisterCommand(const std::string& command, const std::string& onSuccesLog, bool* value);
+
     void AddLog(const char* fmt, spdlog::level::level_enum level);
     void ClearLog();
 
-    void OnImGuiRender(ImGuiWindowFlags windowFlags = ImGuiWindowFlags_NoNavInputs | ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoCollapse);
+    void OnImGuiRender(
+      ImGuiWindowFlags windowFlags = ImGuiWindowFlags_NoNavInputs | ImGuiWindowFlags_MenuBar |
+                                     ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoCollapse);
   private:
     struct ConsoleText {
       std::string Text = {};
@@ -30,7 +48,10 @@ namespace Oxylus {
     };
 
     struct ConsoleCommand {
-      std::function<void()> Action = {};
+      int32_t* IntValue = nullptr;
+      std::string* StrValue = nullptr;
+      bool* BoolValue = nullptr;
+      std::function<void()> Action = nullptr;
       std::string OnSuccesLog = {};
     };
 
@@ -38,11 +59,31 @@ namespace Oxylus {
     std::unordered_map<std::string, ConsoleCommand> m_CommandMap;
     void ProcessCommand(const std::string& command);
 
+    void HelpCommand();
+
+    struct ParsedCommandValue {
+      std::string StrValue = {};
+
+      ParsedCommandValue(std::string str) : StrValue(std::move(str)) { }
+
+      template<typename T= int32_t>
+      std::optional<T> As() const {
+        T value = {};
+        if (std::from_chars(StrValue.c_str(), StrValue.data() + StrValue.size(), value).ec == std::errc{})
+          return (T)value;
+        return {};
+      }
+    };
+
+    ParsedCommandValue ParseValue(const std::string& command);
+    std::string ParseCommand(const std::string& command);
+
     // Input field
+    const int32_t MAX_TEXT_BUFFER_SIZE = 32;
     int32_t m_HistoryPosition = 0;
     std::vector<ConsoleText> m_TextBuffer = {};
     std::vector<char*> m_InputLog = {};
-    bool m_RequestScrollToBottom = false;
+    bool m_RequestScrollToBottom = true;
     int InputTextCallback(ImGuiInputTextCallbackData* data);
   };
 }
